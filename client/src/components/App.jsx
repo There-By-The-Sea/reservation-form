f81bdce56cf53fad953a80b9853fe2f6b3c1ff4a
Trevor Bell <trevor2355@gmail.com>
2020-03-10 19:31:38 -0600

    initial setup of package.json, index.html, webpack, and index.jsx

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <h2>Reservation Form</h2>
+  }
+}
+
+export default App
