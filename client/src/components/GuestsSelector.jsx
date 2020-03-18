e959e1441f36b78cec62bcd47211a27a700bb28c
Trevor Bell <trevor2355@gmail.com>
2020-03-17 19:39:33 -0600

    the dropdown is now fully functionally and appears and dissapears with appropiate clicks

--- client/src/components/GuestsSelector.jsx
+++ client/src/components/GuestsSelector.jsx
@@ -1,21 +1,81 @@
 import React from 'react';
-import SelctionDropDown from './SelectionDropDown.jsx'
-
 import SelectionDropDown from './SelectionDropDown.jsx';
 
-const GuestsSelector = (props) => {
-  return (
-    <div>
-      <div className='GuestsSelector'>
-        <span id='text'>Guests</span>
-        <div className='guestbutton'>
-          <div id='numberofguests'>2 guests</div>
-          <div id='downarrow'>v</div>
+class GuestsSelector extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      adults: 1,
+      children: 3,
+      infants: 0,
+      displayDropDown: false
+    }
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.displayDropDown = this.displayDropDown.bind(this);
+  }
+
+  handleIncrement(event) {
+    var age = event.target.id;
+    var currentNumberPlusOne = this.state[age] + 1
+    console.log(currentNumberPlusOne);
+    this.setState({
+      [age]: currentNumberPlusOne
+    })
+  }
+
+  handleDecrement(event) {
+    var age = event.target.id;
+    var currentNumberPlusOne = this.state[age] - 1
+    console.log(currentNumberPlusOne);
+    this.setState({
+      [age]: currentNumberPlusOne
+    })
+  }
+
+  displayDropDown() {
+    console.log('clicked')
+    this.setState({
+      displayDropDown: !this.state.displayDropDown
+    })
+  }
+
+
+
+  render() {
+    var infantCount = ''
+    if (this.state.infants > 0) {
+      infantCount = ', ' + this.state.infants.toString() + ' Infants'
+    }
+    var dropDown = null;
+    if (this.state.displayDropDown) {
+      dropDown =(
+        <SelectionDropDown 
+        maxGuests={this.props.maxGuests} 
+        adults={this.state.adults} 
+        children={this.state.children} 
+        infants={this.state.infants}
+        handleIncrement={this.handleIncrement}
+        handleDecrement={this.handleDecrement}
+        displayDropDown={this.displayDropDown}
+        />
+      )
+    }
+    return (
+      <div>
+        <div className='GuestsSelector'>
+          <span id='text'>Guests</span>
+          <div className='guestbutton' onClick={this.displayDropDown}>
+            <div id='numberofguests'>{this.state.adults + this.state.children} guests{infantCount}</div>
+            <div id='downarrow'>v</div>
+          </div>
+            {dropDown}
         </div>
       </div>
-      <SelectionDropDown maxGuests={props.maxGuests} />
-    </div>
-  )
+    )
+  }
+
 }
 
 
