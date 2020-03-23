828a2213c4095ebe6bf3717728e9a90e5177c65a
Trevor Bell <trevor2355@gmail.com>
2020-03-23 17:31:03 -0600

    made the necessary changes to enable cors and to use the correct port

--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const reservationRoutes = require('./routes/reservationRoutes')
+const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use(cors())
 
 //serve up the react client
 app.use(express.static(__dirname + '/../client/dist'));
